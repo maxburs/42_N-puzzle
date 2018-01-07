5cf7599a00dd698f8d3fee3ddc2ee0801b061621
Maxwell Burson <maxwellburson@gmail.com>
2018-01-07 11:58:38 -0800

    sorta happy with how it opens a file now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+// type State = [[i32]];
+
+
+fn get_puzzle() -> Option<String> {
+    let file_name = match env::args().nth(1) {
+        Some(file_name) => file_name,
+        None => {
+            println!("Please provide file name as program argument.");
+            return None;
+        }
+    };
+    let mut file_contents = String::new();
+    match File::open(& file_name) {
+        Ok(mut file) => file.read_to_string(&mut file_contents).unwrap(),
+        Err(_) => {
+            println!("Failed to open {}", file_name);
+            return None;
+        }
+    };
+    Some(file_contents)
+}
+
 fn main() {
-    println!("Hello, world!");
+    match get_puzzle() {
+        Some(file) => println!("file: {}", file),
+        None => return
+    }
 }
