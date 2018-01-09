8a9d2f4d3e0e2234ec24032a23a2cd9e0c18275c
Maxwell Burson <maxwellburson@gmail.com>
2018-01-09 14:20:19 -0800

    project is organized enough i think

--- src/puzzle/mod.rs
+++ src/puzzle/mod.rs
@@ -1,18 +1,40 @@
 use std::fmt;
 
 mod parse;
+mod solve;
 
 pub struct Puzzle {
     size: usize,
     content: Vec<Vec<i32>>,
+}
+
+pub struct Solution {
+    // Total number of states ever selected in the "opened" set (complexity in time).
+    complexity_time: u32,
+    // Maximum number of states ever represented in memory at the same time
+    // during the search (complexity in size)
+    complexity_space: u32,
+    // Number of moves required to transition from the initial state to the final state,
+    // according to the search.
+    moves: (), // unimplemented
 }
    
 pub fn new(raw: &str) -> Result<Puzzle, String> {
     parse::parse_puzzle(raw)
 }
 
+trait Solve {
+    fn solve(&self) -> Result<Solution, ()>;
+}
+
 impl fmt::Display for Puzzle {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
        write!(f, "{:?}", self.content)
+    }
+}
+
+impl Solve for Puzzle {
+    fn solve(&self) -> Result<Solution, ()> {
+        solve::solve(self)
     }
 }
---
+++ src/puzzle/solve.rs
@@ -0,0 +1,13 @@
+use puzzle::Puzzle;
+use puzzle::Solution;
+
+pub fn solve(start: &Puzzle) -> Result<Solution, ()> {
+    
+
+
+    Ok(Solution {
+        complexity_time: 0,
+        complexity_space: 0,
+        moves: ()
+    })
+}
